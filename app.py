0f882722cf1b95a4f786d4dd53d302b60ae1a6a0
Ronan Lambert <rflambert@live.co.uk>
2020-06-22 02:43:15 +1200

    Implemented storage of prime numbers

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import time
 
+import json
 import redis
 from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+# Set redis storage with an empty json list
+cache.set('primes',json.dumps([]))
 
 
 def get_hit_count():
@@ -24,13 +27,17 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+# Output if the given number is a prime or not
 @app.route('/isPrime/<int:number>')
 def isPrime(number):
     if number <= 3:
         if number > 1:
+            # 2 or 3, is a prime
+            storePrime(number)
             return '%d is prime' % number
         else:
             return '%d is not prime' % number
+    # Check for every possible factor using the 6k+i optimisation
     elif number % 2 == 0 or number % 3 == 0:
         return '%d is not prime' % number
     i = 5
@@ -38,5 +45,29 @@ def isPrime(number):
         if number % i == 0 or number % (i + 2) == 0:
             return '%d is not prime' % number
         i += 6
+    # No factors found, is a prime
+    storePrime(number)
     return '%d is prime' % number
-        
+
+# Append given number to the json list stored in redis
+def storePrime(number):
+    # Retrieve list of prime numbers from redis
+    primes = json.loads(cache.get('primes'))
+    # Update redis storage with new list
+    primes.append(number)
+    primes.sort()
+    cache.set('primes',json.dumps(primes))
+
+# Output all prime numbers stored in redis
+@app.route('/primesStored')
+def primesStored():
+    primes = json.loads(cache.get('primes'))
+    # Check if list is empty
+    if len(primes) == 0:
+        return 'No primes are stored'
+    # Return a string of all elements in the list
+    output = ''
+    for prime in primes:
+        output += str(prime) + ', '
+    return output
+    
